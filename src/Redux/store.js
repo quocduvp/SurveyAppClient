73973bfac6ab8c70b4dc2017191adb1a39f57ddd
Quoc Du <quocduby@gmail.com>
2018-07-30 00:56:59 +0700

    am

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,9 @@
 import {createStore,applyMiddleware} from 'redux'
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 import promise from 'redux-promise'
 import thunk from 'redux-thunk'
 import {root} from "./rootReducer";
-
-const middleWare = applyMiddleware(thunk,logger,promise);
+// const middleWare = applyMiddleware(thunk,logger,promise);
+const middleWare = applyMiddleware(thunk,promise);
 
 export const store = createStore(root,middleWare)
