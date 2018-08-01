acfe3dfe8226d0af64fbe21911feade59d986ee3
Quoc Du <quocduby@gmail.com>
2018-08-01 10:26:54 +0700

    Final

--- src/API/URL.js
+++ src/API/URL.js
@@ -1,22 +1,22 @@
-export const post_login = 'https://survey-service-api.azurewebsites.net/api/v1/login'
-export const post_register = 'https://survey-service-api.azurewebsites.net/api/v1/register'
-export const get_details_account = 'https://survey-service-api.azurewebsites.net/api/v1/user/profile'
-export const forgot_password = 'https://survey-service-api.azurewebsites.net/api/v1/user/report_account'
-export const edit_my_profile = "https://survey-service-api.azurewebsites.net/api/v1/user/profile"
-export const upload_avatar = "https://survey-service-api.azurewebsites.net/api/v1/upload"
-export const reset_my_password = "https://survey-service-api.azurewebsites.net/api/v1/user/reset_account"
+export const post_login = 'http://localhost:62545/api/v1/login'
+export const post_register = 'http://localhost:62545/api/v1/register'
+export const get_details_account = 'http://localhost:62545/api/v1/user/profile'
+export const forgot_password = 'http://localhost:62545/api/v1/user/report_account'
+export const edit_my_profile = "http://localhost:62545/api/v1/user/profile"
+export const upload_avatar = "http://localhost:62545/api/v1/upload"
+export const reset_my_password = "http://localhost:62545/api/v1/user/reset_account"
 //surveys
-export const fetch_list_survey_incomming = "https://survey-service-api.azurewebsites.net//api/v1/user/surveys_incomming"
-export const fetch_list_survey_uncomming = "https://survey-service-api.azurewebsites.net/api/v1/user/surveys_uncomming"
-export const fetch_survey_details = "https://survey-service-api.azurewebsites.net//api/v1/survey/response"
-export const submit_join_surveys =  "https://survey-service-api.azurewebsites.net/api/v1/survey/join"
-export const submit_answer_for_question =  "https://survey-service-api.azurewebsites.net/api/v1/survey/join"
-export const list_survey_submitted = "https://survey-service-api.azurewebsites.net/api/v1/survey_response"
-export const details_survey_submitted = "https://survey-service-api.azurewebsites.net/api/v1/survey_response"
+export const fetch_list_survey_incomming = "http://localhost:62545//api/v1/user/surveys_incomming"
+export const fetch_list_survey_uncomming = "http://localhost:62545/api/v1/user/surveys_uncomming"
+export const fetch_survey_details = "http://localhost:62545/api/v1/survey/response"
+export const submit_join_surveys =  "http://localhost:62545/api/v1/survey/join"
+export const submit_answer_for_question =  "http://localhost:62545/api/v1/survey/join"
+export const list_survey_submitted = "http://localhost:62545/api/v1/survey_response"
+export const details_survey_submitted = "http://localhost:62545/api/v1/survey_response"
 //faqs
-export const details_faqs = "https://survey-service-api.azurewebsites.net/api/v1/all/faqs"
-export const list_faqs = "https://survey-service-api.azurewebsites.net/api/v1/all/faqs"
+export const details_faqs = "http://localhost:62545/api/v1/all/faqs"
+export const list_faqs = "http://localhost:62545/api/v1/all/faqs"
 //feedback
-export const submit_feedback = "https://survey-service-api.azurewebsites.net/api/v1/user/feedback"
+export const submit_feedback = "http://localhost:62545/api/v1/user/feedback"
 //classroom
-export const get_list_classrooms = "https://survey-service-api.azurewebsites.net/api/v1/all/classroom"
+export const get_list_classrooms = "http://localhost:62545/api/v1/all/classroom"
